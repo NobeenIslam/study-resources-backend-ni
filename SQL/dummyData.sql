1ddaa70b468ce936d14dd8d461e866c9626e69e1
Nobeen <n.islam.1@outlook.com>
2022-05-11 09:34:41 +0100

    Resources dummy in one file, linked some of the other dummy data to new resources

--- SQL/dummyData.sql
+++ SQL/dummyData.sql
@@ -1,125 +1,16 @@
 INSERT INTO users (name,is_faculty)
-VALUES ('James', true),('Blobs',false),('Michael Jackson', false),('Donald Trump', true);
-
-INSERT INTO resources (
-  title,
-  description,
-  url,
-  origin,
-  author_id,
-  votes,
-  content_type,
-  recommended_week,
-  evaluation,
-  justification
-)
-VALUES ('book','this is a book','https.google.com','nobeen',1,0,'book','week1','I recommend this resource after having used it','This book changed my life. I can now make cookies');
+VALUES ('James', true),('Blobs',false),('Michael Jackson', false),('Donald Trump', true),('Owenster', true);
 
 INSERT INTO comments(body,author_id,resource_id)
-VALUES ('Siiiiiick',1,1),('Whats in the baaaaax',1,1);
+VALUES ('Siiiiiick',1,1),('Whats in the baaaaax',1,3);
 
 INSERT INTO study_list (author_id,resource_id)
-VALUES (1,1),(2,1)(3,1)(4,1);
+VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6);
 
 INSERT INTO tags (name, resource_id)
-VALUES ('react',1),('JS',1),('creative coding', 1),('git',1),('SQL',1),('express',1);
+VALUES ('react',1),('JS',2),('creative coding', 3),('git',1),('SQL',1),('express',1);
 
 
-INSERT INTO resources (
-  title,
-  description,
-  url,
-  origin,
-  author_id,
-  content_type,
-  recommended_week,
-  evaluation,
-  justification
-) VALUES
-(
-    'book',
-    'this is a book',
-    'https.google.com',
-    'nobeen',
-    1,
-    'book',
-    'week1',
-    'This is a good resource',
-    'I used it and it was crap'
-);
-(
-    'Vectors and the essence of linear algebra', 
-    'an introduction to vectors. Start of a series of videos on linear algebra.',
-    'https://www.youtube.com/watch?v=fNk_zzaMoSs&ab_channel=3Blue1Brown',
-    '3Blue1Brown',
-    3,
-    'Youtube Video',
-    'Creative Coding wk 3',
-    'I haven''t used this resource but it looks promising',
-    'Having not done a maths degree of A-levels, I struggled to get up to speed on using vectors and this could prove useful'
-);
-(
-    'Replit sandbox',
-    'a useful sandbox for writing quick code',
-    'https://replit.com/~',
-    'Replit',
-    1,
-    'Website',
-    'All Weeks',
-    'I recommend this resource after having used it',
-    'thought it would be good to have quick access to this for little projects and working out small algorithms'
-);
-(
-    'Introduction to TKinter',
-    'article introducing the TKinter GUI for Python and how to set it up',
-    'https://www.datacamp.com/tutorial/gui-tkinter-python',
-    'DataCamp'
-    3,
-    'Website',
-    null,
-    'I recommend this resource after having used it',
-    'simple looking gui but good as an intro and helped make a program that didnt rely on the console'
-);
-(
-    'Academy Cohort 4 Build Page',
-    'Notion page storing all the useful information for C4 of Academy',
-    'https://www.notion.so/weareacademy/Cohort-4-Build-f62a971a774445b085f9db89a168baee',
-    'Notion, Academy'
-    2,
-    'All Weeks',
-    'I recommend this resource after having used it',
-    'thought would be good to keep the link safe'
-);
-(
-    'SQL Cheatsheet',
-    'contains useful information for making queries in SQL',
-    'https://s3-us-west-2.amazonaws.com/dbshostedfiles/dbs/sql_cheat_sheet_pgsql.pdf',
-    'www.databasestar.com',
-    4,
-    'SQL Week 1 - 3',
-    'I haven''t used this resource but it looks promising',
-    'it''s really hard to remember all the SQL queries and this could be good if you need a refresher'
-);
-(
-    'Intro to P5JS',
-    'Shiffman video containing a useful introduction to p5js',
-    'https://www.youtube.com/watch?v=8j0UDiN7my4&ab_channel=TheCodingTrain',
-    'The Coding Train, Daniel Shiffman',
-    1,
-    'Creative Coding Week 1',
-    'I haven''t used this resource but it looks promising',
-    null
-);
-(
-    'pics of dogs',
-    'for a pick-me-up if the coding world is getting too much',
-    'https://www.akc.org/expert-advice/lifestyle/35-perfect-pictures-of-dogs/',
-    'American Kennel Club',
-    5,
-    'All Weeks',
-    'I do not recommend this resource, having used it',
-    'cats >'
-);
 
 
 
